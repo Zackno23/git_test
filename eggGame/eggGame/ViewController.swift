28bdcd586bfa3631ced1632c4e16ddde21a4d5b4
Chikara Yoshida <chikara.f.yoshida@gmail.com>
2019-06-02 22:39:31 +0900

    eggGame要件達成 各数字に対し」メッセージを表示

--- eggGame/eggGame/ViewController.swift
+++ eggGame/eggGame/ViewController.swift
@@ -11,10 +11,32 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        countLabel.text = String(number)
+        textLabel.text = text
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var number = 10
+    var text = "ゲーム開始"
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBAction func eggButton(_ sender: UIButton) {
+        if countLabel.text != "1"{
+            switch countLabel.text{
+            case "5":
+                textLabel.text = "いいね"
+            case "3":
+                textLabel.text = "あとちょっと"
+            default:
+                textLabel.text = ""
+            }
+            number -= 1
+            countLabel.text = String(number)
+        }else{
+            countLabel.text = "0"
+            textLabel.text = "ゲーム終了"
+        }
+    }
+    
 }
 
