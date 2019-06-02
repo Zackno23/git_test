0da0b476266ee596123d896185b09942e31ea118
Chikara Yoshida <chikara.f.yoshida@gmail.com>
2019-06-02 22:04:42 +0900

    First Commit

---
+++ eggGame/eggGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eggGame
+//
+//  Created by 吉田力 on 2019/06/02.
+//  Copyright © 2019 吉田力. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
