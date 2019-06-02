8c8cc4c3af8a0e79ea3a5e2e4ea9a45cf06d04e3
Chikara Yoshida <chikara.f.yoshida@gmail.com>
2019-06-03 02:42:02 +0900

    昇竜拳成功!! DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {                 self.ryu.image = UIImage(named: "attak\(i)")

--- syoryuken/syoryuken/ViewController.swift
+++ syoryuken/syoryuken/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let attackImage = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ryu.image = UIImage(named: "attak1")
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var ryu: UIImageView!
+    @IBAction func command(_ sender: Any) {
+        for i in 1 ... 20{
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.25 * Double(i)) {
+                self.ryu.image = UIImage(named: "attak\(i)")
+            }
+        }
+    }
+    
 }
 
