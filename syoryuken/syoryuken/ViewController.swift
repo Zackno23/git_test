04a1d185f88c821595e2634d8ea5314d299122b7
Chikara Yoshida <chikara.f.yoshida@gmail.com>
2019-06-04 15:23:04 +0900

    「昇竜拳!」ボイス追加!

--- syoryuken/syoryuken/ViewController.swift
+++ syoryuken/syoryuken/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate{
     let attackImage = UIImageView()
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +21,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var ryu: UIImageView!
     @IBAction func command(_ sender: Any) {
+        let audioPath = Bundle.main.path(forResource: "nc177750", ofType: "wav")!
+        let audioUrl = URL(fileURLWithPath: audioPath)
+        var audioError: NSError?
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioUrl)
+        }catch let error as NSError{
+            audioError = error
+            audioPlayer = nil
+            if let error = audioError{
+                print("Error \(error.localizedDescription)")
+            }
+            audioPlayer.delegate = self
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        }
         for i in 1 ... 20{
             DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1 * Double(i)) {
                 self.ryu.image = UIImage(named: "attak\(i)")
             }
         }
+        audioPlayer.play()
     }
     
 }
